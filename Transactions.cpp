b7520620047614db76c1c5a189531b81a8263e9a
bevan Donbosco <bevan2424@hotmail.com>
2021-02-22 17:37:59 -0500

    updated transactions

--- Transactions.cpp
+++ Transactions.cpp
@@ -5,43 +5,43 @@ class Transaction{
 public:
   Transaction();
 
-  bool login(std::string accountNumber, std::string accountId){
-    if(FileIO.accountExists(accountNumber, accountId)){
+  bool login(Account acc){
+    if(acc.accountExists(acc.accountNumber, acc.accountId)){
       return true;
     }
     return false;
   }
 
-  void withdrawl(std::string accountHolderName,std::string accountId,float withdrawAmount,float accountBalance){
-    if(FileIO.accountExists(accountHolderName, accountId)){
-      if(withdrawAmount > 0.0 && withdrawAmount <= 500.0){
+  void withdrawl(Account acc){
+    if(acc.accountExists(acc.accountHolderName, acc.accountId)){
+      if(acc.withdrawAmount > 0.0 && acc.withdrawAmount <= 500.0){
         //TODO:
         //stuff here
       }
     }
   }
 
-  void deposit(std::string accountHolderName,std::string accountId,float depositAmount){
-    if(FileIO.accountExists(accountHolderName,accountId)){
+  void deposit(Account acc){
+    if(acc.accountExists(acc.accountHolderName,acc.accountId)){
       //funds transfers cannot be used in session
     }
   }
 
-  void transfer(std::string accountHolderName,std::string accountId,std::string recieverId,float transferAmount){
-    if(FileIO.accountExists(accountHolderName,accountId)){
+  void transfer(Account acc){
+    if(acc.accountExists(acc.accountHolderName,acc.accountId)){
       //funds transfers cannot be used in session
-      if(transferAmount > 0 && transferAmount < 1000){
+      if(acc.transferAmount > 0 && acc.transferAmount < 1000){
         //modify account amounts
       }
     }
   }
 
-  void paybill(std::string accountHolderName,std::string accountId,float amountToPay,float accountBalance,std::string companyName){
-    if(FileIO.accountExists(accountHolderName,accountId)){
+  void paybill(Account acc){
+    if(acc.accountExists(acc.accountHolderName,(acc.accountId)){
       //funds transfers cannot be used in session
-      if(amountToPay > 0 && accountBalance < 2000){
+      if(acc.amountToPay > 0 && acc.accountBalance < 2000){
         //modify account amounts
-        if(companyName == "The Bright Light Electric Company(EC)"||"Credit Card Company Q(CQ)"
+        if(acc.companyName == "The Bright Light Electric Company(EC)"||"Credit Card Company Q(CQ)"
         ||"Fast Internet, Inc.(FI)"){
           //update account bill payment
         }
@@ -49,40 +49,40 @@ public:
     }
   }
 
-  void create(std::string accountHolderName,float intialBalance,bool isAdmin){
-    if(isAdmin == true && intialBalance < 999999.99 && accountHolderName.length() < 20 ){
+  void create(Account acc){
+    if(acc.isAdmin == true && acc.intialBalance < 999999.99 && acc.accountHolderName.length() < 20 ){
       // fileIO create new bank account with initial balance
     }
   }
 
-  void deleting(std::string accountHolderName,std::string accountId,bool isAdmin,std::string verifyDelete){
-    if(isAdmin == true){
-      if(FileIO.accountExists(accountHolderName,accountId)){
-        if(accountId==verifyDelete){
+  void deleting(Account acc){
+    if(acc.isAdmin == true){
+      if(acc.accountExists(acc.accountHolderName,acc.accountId)){
+        if(acc.accountId==acc.verifyDelete){
           // delete in fileIO
         }
       }
     }
   }
 
-  void disable(std::string accountHolderName,std::string accountId,bool isAdmin,std::string verifyDisable){
-    if(FileIO.accountExists(accountHolderName,accountId){
-      if(isAdmin == true)){
-        if(accountId==verifyDisable){
+  void disable(Account acc){
+    if(acc.accountExists(acc.accountHolderName,acc.accountId){
+      if(acc.isAdmin == true){
+        if( acc.accountId == acc.verifyDisable ){
           //fileIO
         }
       }
     }
   }
 
-  void changeplan(std::string accountHolderName,std::string accountId,bool studentPlan,bool isAdmin){
-    if(FileIO.accountExists(accountHolderName,accountId){
-      if(isAdmin == true)){
-        if(studentPlan == true){
+  void changeplan(Account acc){
+    if(acc.accountExists(acc.accountHolderName,acc.accountId){
+      if(acc.isAdmin == true)){
+        if(acc.studentPlan == true){
           //output as studentplan
         }
       }
     }
   }
 
-;}
+};
