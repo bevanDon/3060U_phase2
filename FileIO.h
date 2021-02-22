4bb80caef879986f540f82902c1744ece92e422f
Ed Liu <jia.liu@ontariotechu.net>
2021-02-22 18:27:27 -0500

    updated fileIO headerfile

--- FileIO.h
+++ FileIO.h
@@ -12,27 +12,7 @@ public:
      // methods for banking transactions
     FileIO();
     void login(std::string accountNumber, std::string accountId);
-    void withdrawal(std::string accountHolderName,std::string accountId,float withdrawAmount);
-    void deposit(std::string accountHolderName,std::string accountId,float depositAmount);
-    void transfer(std::string accountHolderName,std::string accountId,std::string recieverId,float transferAmount);
-    void paybill(std::string accountHolderName,std::string accountId,float amountToPay);
-    void create(std::string accountHolderName,float intialBalance);
-    void deleting(std::string accountHolderName,std::string accountId);
-    void disable(std::string accountHolderName,std::string accountId);
-    void changeplan(std::string accountHolderName,std::string accountId);
-    /*                                                  //Default empty constructor for fileIO, defines text files
-    void setIteration(std::string iteration);
-    void setUserData(std::string currentUsers);
-    void setAvailableItems(std::string availables);
+    bool accountExists(Account acc);
+    void updateMoney(Account acc, float money);
 
-    bool logUserIn(std::string username);                           //function to read user accounts file to check if user exist
-    std::string getAccountType(std::string username);                      //function to read user accounts file to check the user account types
-    float getUserCredit(std::string username);                      //function to read user accounts file to view user's credit
-    void setUserCredit(std::string username, float newCredit);      //function to read user accounts file to add credit to user
-    bool createNewUser(std::string username, std::string type);     //function to read user accounts file to add new user in create()
-    bool deleteUser(std::string username);                          //function to read user accounts file to delete current user in deleteUser()
-    void generateDailyTransaction(std::string log);                 //function to append transaction log to file
-    bool itemExists(std::string username, std::string itemName);
-    void refundUser(std::string buyer, std::string seller, float credit);
-    */
 };
